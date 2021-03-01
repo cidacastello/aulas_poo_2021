4b9365c76c609ffe074bc3ac195718e6a95581aa
Cida Castello <cidacastello.cursos@gmail.com>
2021-03-01 11:57:18 -0300

    Hello World com .Net Core

---
+++ Programa2_NetCore/Programa2_NetCore/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Programa2_NetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Projeto com .Net Core");
+            Console.ReadKey();
+        }
+    }
+}
