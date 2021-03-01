c3f84a23179aa3b1968a41eefe84e4e443136e01
Cida Castello <cidacastello.cursos@gmail.com>
2021-03-01 11:52:35 -0300

    HelloWorld  com .Net Framework- primeiro programa

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //saída em uma janela de console (prompt de comando)
+            Console.WriteLine("Hello World!!");
+            Console.ReadKey();
+        }
+    }
+}
